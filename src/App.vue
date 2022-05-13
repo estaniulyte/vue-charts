cf06523217a10d5cb824112f0068ff1d6c18291b
Evelina Staniulytė <e.staniulyte98@gmail.com>
2022-05-13 13:38:05 +0300

    Add Doughnut Chart

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DoughnutChart />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import DoughnutChart from './components/DoughnutChart.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+    DoughnutChart,
   }
 }
 </script>
---
+++ src/components/DoughnutChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <DoughnutChart :chartData="testData" />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { DoughnutChart } from 'vue-chart-3';
+import { Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
+
+export default defineComponent({
+  name: 'donught-chart',
+  components: { DoughnutChart },
+  setup() {
+    const testData = {
+      labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre', 'kk'],
+      datasets: [
+        {
+          data: [30, 40, 60, 70, 5, 60],
+          backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED', '#75C8ED'],
+        },
+      ],
+    };
+
+    return { testData };
+  },
+});
+</script>
